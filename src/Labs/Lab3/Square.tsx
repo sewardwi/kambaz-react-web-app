3a028212648c5db23678dd2b27fd7bb192d2ce72
William Seward <wdseward03@gmail.com>
2025-05-26 22:25:24 -0400

    remove unused react import

--- src/Labs/Lab3/Square.tsx
+++ src/Labs/Lab3/Square.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode } from "react";
+import { type ReactNode } from "react";
 export default function Square({ children }: { children: ReactNode }) {
   const num = Number(children);
   return <span id="wd-square">{num * num}</span>;
